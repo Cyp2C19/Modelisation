07aebeff6a6e0264c1d73e1cc89853e7fd079d17
Cyp2C19 <cyprien8@hotmail.fr>
2017-10-12 10:13:30 +0200

    Creation des tranches

--- densityProfile.py
+++ densityProfile.py
@@ -5,8 +5,15 @@ import mdtraj as md
 traj = md.load('C:\Users\cyprien\Desktop\M2\Modelisation\p3_p4_p5_p8\start.pdb')
 topology = traj.topology
 
+listZ = traj.xyz[0, : , 2]
+minZ = min(listZ)
+maxZ = max(listZ)
 
-def numberOfMoleculesPerSlice(slices):
+print(minZ)
+print(maxZ)
+
+def numberOfMoleculesPerSlice():
+
     residues = {}
     for residue in topology.residues:
         id = residue.name + str(residue.resSeq)
@@ -19,11 +26,26 @@ def numberOfMoleculesPerSlice(slices):
             residues[id].append(z)
     return(residues)
 
+def makeSlices(nbSlices):
+    zRange = maxZ/nbSlices
+    zList = [minZ]
+    z = zRange
+
+    while z < maxZ:
+        z += zRange
+        zList.append(z)
+    zList.append(maxZ)
+
+    return(zList)
+
+
 # for key, value in residues.items():
 #     print(key)
 #     print(value)
 
-residues = numberOfMoleculesPerSlice()
-print(residues['DOP1'])
-print(len(residues))
-print(topology)
+# residues = numberOfMoleculesPerSlice()
+# print(residues['DOP1'])
+# print(len(residues))
+
+z = makeSlices(10)
+print(len(z))
