f8e9bd081e5f3adeac5eb1547863079c1b90f4ad
Cyp2C19 <cyprien8@hotmail.fr>
2018-10-01 16:20:14 +0200

    Update densityProfile.py

--- densityProfile.py
+++ densityProfile.py
@@ -6,58 +6,61 @@ from __future__ import division
 import mdtraj as md
 from mdtraj import element
 import numpy as np
-import operator as op
 import matplotlib.pyplot as plt
 
-# Chargement trajectoires/topologie
-traj = md.load('C:\Users\cyprien\Desktop\M2\Modelisation\p3_p4_p5_p8\md_200ns_OK.xtc',
-               top='C:\Users\cyprien\Desktop\M2\Modelisation\p3_p4_p5_p8\start.pdb')
-topology = traj.topology
-
-def sort_atoms_inside_slices(slices, frame_id):
+def sort_atoms_inside_slices(slices):
     """
-       Fonction qui permet blablablabla
+       Fonction qui traite les frames une à une et qui parcours chaque atome de
+       chaque molécule pour les ranger dans les différentes tranches.
+       @param: slices Tableau contenant les positions z des différentes tranches.
+       @return: h2o, dopc, dope Dictionnaires associant les tranches et les symboles
+       des atomes pour les 3 molécules étudiées.
     """
-
     h2o = {i: [] for i in range(len(slices) + 1)}
     dopc = {i: [] for i in range(len(slices) + 1)}
     dope = {i: [] for i in range(len(slices) + 1)}
 
-    for residue in topology.residues:
-        list_atoms = []
-        is_dope = False
-        for atom in residue.atoms:
-            # Si atome d'H et appartient a un DOP --> DOPE
-            if atom.element.symbol == 'H' and not atom.residue.is_water:
-                is_dope = True
-            # Recuperation pos z atome
-            list_atoms.append((traj.xyz[frame_id, atom.index, 2], atom.element.symbol))
-        # Parcours des tranches pour placer les atomes.
-        # Tranche 1 = z1 --> atomes avec z compris entre z0 (min) et z1.
-        for z in list_atoms:
-            i = 1
-            while z[0] > slices[i] and i < len(slices):
-                i += 1
-            # Atome appartenant à une molecule H2O
-            if atom.residue.is_water:
-                h2o[i].append(z[1])
-            # Atome appartenant a un DOPE
-            elif is_dope:
-                dope[i].append(z[1])
-            # Atome appartenant a un DOPC
-            else:
-                dopc[i].append(z[1])
+    # Pour chaque frame
+    for f in range(frame_nb + 1):
+        print("\nTraitement de la frame : " + str(f))
+        # Pour chaque molécule
+        for residue in topology.residues:
+            list_atoms = []
+            is_dope = False
+            # Pour chaque atome de la molecule courante
+            for atom in residue.atoms:
+                # Si atome d'H et appartient a un DOP --> DOPE
+                if atom.element.symbol == 'H' and not atom.residue.is_water:
+                    is_dope = True
+                # Recuperation pos z atome et symbole de l'élément
+                list_atoms.append((traj.xyz[f, atom.index, 2], atom.element.symbol))
+            # Parcours des tranches pour placer les atomes.
+            # Tranche 1 : z1 --> contient les atomes avec z compris entre z0 (min) et z1.
+            for z in list_atoms:
+                i = 1
+                while z[0] > slices[i] and i < len(slices)- 1:
+                    i += 1
+                # Atome appartenant à une molecule H2O
+                if atom.residue.is_water:
+                    h2o[i].append(z[1])
+                # Atome appartenant a un DOPE
+                elif is_dope:
+                    dope[i].append(z[1])
+                # Atome appartenant a un DOPC
+                else:
+                    dopc[i].append(z[1])
     return(h2o, dopc, dope)
 
 def make_slices(nb_slices):
     """
-       Fonction qui permet blablablabla
+       Fonction qui permet de diviser la longueur de la boîte par le
+       nombre de tranches souhaitées.
+       @param: nb_slices Le nombre de tranches défini par l'utilisateur
+       @return: z_list Les positions z des tranches
     """
-
-    z_range = BOX_LENGTH / nb_slices
+    z_range = Z_BOX / nb_slices
     z_list = []
     z = 0
-
     for i in range(nb_slices + 1):
         z_list.append(z)
         z += z_range
@@ -65,9 +68,13 @@ def make_slices(nb_slices):
 
 def number_of_atoms(h2o, dopc, dope):
     """
-       Fonction qui permet blablablabla
+       Fonction qui permet de compter le nombre d'atomes de h2o, dopc et dope
+       pour chaque tranche.
+       @param: h2o, dopc, dope Dictionnaires associant les tranches et les
+       symboles des atomes pour les 3 molécules étudiées.
+       @return: new_h2o, new_dopc, new_dope Les listes contenant les nombres
+       d'atomes pour chaque tranche.
     """
-
     new_h2o = []
     new_dope = []
     new_dopc = []
@@ -78,40 +85,51 @@ def number_of_atoms(h2o, dopc, dope):
         new_dopc.append(len(dopc[i]))
     return(new_h2o, new_dopc, new_dope)
 
-def mass_density(h2o, dopc, dope):
+def mass_of_atoms(h2o, dopc, dope):
     """
-       Fonction qui permet blablablabla
+       Fonction qui permet de calculer la masse totale des atomes de h2o, dopc, dope
+       contenus dans chaque tranche.
+       @param: h2o, dopc, dope Dictionnaires associant les tranches et les symboles
+       des atomes pour les 3 molécules étudiées.
+       @return: new_h2o, new_dopc, new_dope Les listes contenant la masse totale des
+       atomes pour chaque molécule pour chaque tranche.
     """
-
     new_h2o = []
     new_dope = []
     new_dopc = []
+
     for i in range(nb_slices + 1):
         if len(h2o[i]) > 0:
-            new_h2o.append(density('H', h2o[i].count('H'))
-                           + density('O', h2o[i].count('O')))
+            new_h2o.append(mass('H', h2o[i].count('H'))
+                           + mass('O', h2o[i].count('O')))
         else:
             new_h2o.append(0)
 
         if (len(dope[i]) > 0):
-            new_dope.append(density('C', dope[i].count('C'))
-                            + density('P', dope[i].count('P'))
-                            + density('O', dope[i].count('O'))
-                            + density('N', dope[i].count('N'))
-                            + density('H', dope[i].count('H')))
+            new_dope.append(mass('C', dope[i].count('C'))
+                            + mass('P', dope[i].count('P'))
+                            + mass('O', dope[i].count('O'))
+                            + mass('N', dope[i].count('N'))
+                            + mass('H', dope[i].count('H')))
         else:
             new_dope.append(0)
 
         if (len(dopc[i]) > 0):
-            new_dopc.append(density('C', dopc[i].count('C'))
-                            + density('P', dopc[i].count('P'))
-                            + density('O', dopc[i].count('O'))
-                            + density('N', dopc[i].count('N')))
+            new_dopc.append(mass('C', dopc[i].count('C'))
+                            + mass('P', dopc[i].count('P'))
+                            + mass('O', dopc[i].count('O'))
+                            + mass('N', dopc[i].count('N')))
         else:
             new_dopc.append(0)
     return(new_h2o, new_dopc, new_dope)
 
-def density(element, nb_atoms):
+def mass(element, nb_atoms):
+    """
+       Fonction qui permet de calculer la masse pour un type d'atome.
+       @param: element Le symbole de l'atome
+       @param: nb_atoms Le nombre d'atomes pour l'élément concerné
+       @return: m La masse ramenée en kg
+    """
     n = nb_atoms / NA # Calcul quantité matière
     # Calcul masse
     if element == 'C':
@@ -124,37 +142,76 @@ def density(element, nb_atoms):
         m = n * MN
     elif element == 'H':
         m = n * MH
-    # Calcul densité
-    slice_length = BOX_LENGTH / nb_slices
-    vol = (slice_length**slice_length) * 1E-27 # Volume tranche en m^-3
-    m = m * 1E-3 # Masse en kg
-    d = m / vol # Densité en kg/m^-3
-    return(d)
+    return(m *1E-3)
+
+def density(h2o, dopc, dope):
+    """
+       Fonction qui permet de calculer la densité des h2o, dopc et dope
+       pour chaque tranches.
+       @param: h2o, dopc, dope Les listes contenant les masses totales pour
+       chaque tranche.
+       @return: h2o, dopc, dope Les listes contenant les densités des
+       molécules pour chaque tranche.
+    """
+    # Calcul volume tranche
+    z_slice = Z_BOX / nb_slices
+    x_slice = X_BOX / nb_slices
+    y_slice = Y_BOX / nb_slices
+    vol = (z_slice * x_slice * y_slice) * 1E-27 # Volume tranche en m^-3
+
+    for i in range(nb_slices + 1):
+        # Calcul densité en kg/m^-3
+        h2o[i] = h2o[i] / vol
+        dopc[i] = dopc[i] / vol
+        dope[i] = dope[i] / vol
+    return(h2o, dopc, dope)
+
+def density_mean(h2o, dopc, dope):
+    """
+       Fonction qui permet calculer la moyenne des densités en fonction
+       du nobmre de frame parcourues.
+       @param: h2o, dopc, dope Les listes contenant les densités des
+       molécules pour chaque tranche.
+       @return: h2o, dopc, dope Les listes contenant les densités des
+       molécules moyennées sur le nombre de frames.
+    """
+    for i in range(nb_slices + 1):
+        # Calcul densité en kg/m^-3
+        h2o[i] = h2o[i] / (frame_nb + 1)
+        dopc[i] = dopc[i] / (frame_nb + 1)
+        dope[i] = dope[i] / (frame_nb + 1)
+    return (h2o, dopc, dope)
 
 def number_density(h2o, dopc, dope):
     """
-       Fonction qui permet blablablabla
+       Fonction qui permet calculer la densité en fonction des nombres
+       d'atomes des molécules pour chaque tranche.
+       @param: h2o, dopc, dope Les listes contenant les nombres d'atomes
+       des molécules pour chaque tranche.
+       @return: h2o, dopc, dope Les listes contenant les rapports pour chaque
+       tranche du nombre d'atomes d'une molecule / nombre total d'atomes des 3
+       molécules.
     """
-    new_h2o = []
-    new_dopc = []
-    new_dope = []
-
     for i in range(nb_slices + 1):
         total = h2o[i] + dopc[i] + dope[i]
         if(total == 0):
-            new_h2o.append(0)
-            new_dopc.append(0)
-            new_dope.append(0)
+            h2o[i] = 0
+            dopc[i] = 0
+            dope[i] = 0
         else:
             # Calcul pourcentage nombre d'atomes en fonction total d'atomes
-            new_h2o.append(round(h2o[i] / total, 4) * 100)
-            new_dopc.append(round(dopc[i] / total, 4) * 100)
-            new_dope.append(round(dope[i] / total, 4) * 100)
-    return(new_h2o, new_dopc, new_dope)
+            h2o[i] = round(h2o[i] / total, 4) * 100
+            dopc[i] = round(dopc[i] / total, 4) * 100
+            dope[i] = round(dope[i] / total, 4) * 100
+    return(h2o, dopc, dope)
 
 def draw_density_profile(h2o, dopc, dope, mass_density):
     """
-       Fonction qui permet blablablabla
+       Méthode qui permet de tracer les profils de densités.
+       @param: h2o, dopc, dope Les listes contenant les densités massiques/
+       nombres d'atomes des molécules pour chaque tranche.
+       @param: mass_density Permet de tracer la courbe en fonction des densités si vrai,
+       sinon trace en fonction du nombre d'atomes.
     """
     # Gestion si aucun atome dans une tranche --> None : on ne trace pas de point
     for i in range(nb_slices + 1):
@@ -167,8 +224,10 @@ def draw_density_profile(h2o, dopc, dope, mass_density):
     plt.plot(h2o, y, label="H2O")
     plt.plot(dopc, y, label="DOPC")
     plt.plot(dope, y, label="DOPE")
-    plt.xlabel("Densite (kg/m^-3)") if mass_density else plt.xlabel("Densite en nombre d'atomes (%)")
+    plt.xlabel("Densite (kg/m^-3)") if mass_density \
+        else plt.xlabel("Densite en nombre d'atomes (%)")
     plt.ylabel("Boite (nm)")
+
     plt.legend()
     plt.show()
 
@@ -178,12 +237,22 @@ MH = element.hydrogen.mass # Masse molaire Hydrogène
 MP = element.phosphorus.mass # Masse molaire Phosphore
 MN = element.nitrogen.mass # Masse molaire Azote
 MC = element.carbon.mass # Masse molaire Carbone
-NA = 6.022e23 # Constante d'Avogadro
+NA = 6.022E23 # Constante d'Avogadro
 
-BOX_LENGTH = 9.0 # 9 nm de longueur en x, y et z
+X_BOX = 9.2042 # 9 nm de longueur en x, y et z
+Y_BOX = 9.2042
+Z_BOX = 7.8801
 
+# Récupérer toutes les frames : topology.n_frames
+# Ici le nombre est défini en brut pour des questions de performances.
+frame_nb = 100
 
-frame_nb = 5
+# Chargement trajectoires/topologie
+xtc = raw_input("Entrez le chemin de votre fichier xtc => ")
+pdb = raw_input("\nEntrez le chemin de votre fichier pdb => ")
+print("\nChargement des fichiers en cours ...")
+traj = md.load(xtc, top=pdb)
+topology = traj.topology
 
 #─────────────────────────────────────── MAIN ─────────────────────────────────────────#
 
@@ -199,35 +268,23 @@ while(run):
 
     if choix == '1':
         nb_slices = input("\nNombre de tranches pour la division de la boîte => ")
-        h2o_final = [0] * (nb_slices + 1)
-        dopc_final = [0] * (nb_slices + 1)
-        dope_final = [0] * (nb_slices + 1)
-        slices = make_slices(nb_slices)  # Découpage de la boite en tranches
-
+        slices = make_slices(nb_slices)
         print("\nConstruction du graphique en cours ...")
-        for i in range(frame_nb + 1):
-            h2o, dopc, dope = sort_atoms_inside_slices(slices, i)
-            h2o, dopc, dope = mass_density(h2o, dopc, dope)
-            h2o_final = map(op.add, h2o_final, h2o)
-            dopc_final = map(op.add, dopc_final, dopc)
-            dope_final = map(op.add, dope_final, dope)
-        draw_density_profile(h2o_final, dopc_final, dope_final, True)
+
+        h2o, dopc, dope = sort_atoms_inside_slices(slices)
+        h2o, dopc, dope = mass_of_atoms(h2o, dopc, dope)
+        h2o, dopc, dope = density(h2o, dopc, dope)
+        h2o, dopc, dope = density_mean(h2o, dopc, dope)
+        draw_density_profile(h2o, dopc, dope, True)
 
     elif choix == '2':
         nb_slices = input("\nNombre de tranches pour la division de la boîte => ")
-        h2o_final = [0] * (nb_slices + 1)
-        dopc_final = [0] * (nb_slices + 1)
-        dope_final = [0] * (nb_slices + 1)
-        slices = make_slices(nb_slices)  # Découpage de la boite en tranches
-
+        slices = make_slices(nb_slices)
         print("\nConstruction du graphique en cours ...")
-        for i in range(frame_nb + 1):
-            h2o, dopc, dope = sort_atoms_inside_slices(slices, i)
-            h2o, dopc, dope = number_of_atoms(h2o, dopc, dope)
-            h2o_final = map(op.add, h2o_final, h2o)
-            dopc_final = map(op.add, dopc_final, dopc)
-            dope_final = map(op.add, dope_final, dope)
-        h2o, dopc, dope = number_density(h2o_final, dopc_final, dope_final)
+
+        h2o, dopc, dope = sort_atoms_inside_slices(slices)
+        h2o, dopc, dope = number_of_atoms(h2o, dopc, dope)
+        h2o, dopc, dope = number_density(h2o, dopc, dope)
         draw_density_profile(h2o, dopc, dope, False)
 
     elif choix == '3':
